a9095094f1e4d6f35a61db19107ea3abd7071340
changbo <446563968@qq.com>
2017-12-13 15:07:13 +0800

    新增monitor模块

---
+++ zk_task.monitor/src/main/java/com/bj58/lbg/zk_task/monitor/MonitorApplication.java
@@ -0,0 +1,10 @@
+package com.bj58.lbg.zk_task.monitor;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class MonitorApplication {
+
+}
